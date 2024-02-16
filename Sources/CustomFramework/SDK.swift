72bd5df68d991f75e97b4d2af1fb05cd555f855c
DieterM <dieterm@velocitycubed.com>
2024-02-16 18:37:57 +0800

    First Commit

---
+++ Sources/CustomFramework/SDK.swift
@@ -0,0 +1,14 @@
+//
+//  SDK.swift
+//  
+//
+//  Created by user on 2024/02/16.
+//
+
+import Foundation
+
+public struct SDK{
+    public static func doSomeWork() {
+        print("Doing some work ...")
+    }
+}
